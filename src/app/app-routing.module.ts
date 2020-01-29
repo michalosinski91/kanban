dcacb417e5f952b3660808bae44782ee60455e07
michalosinski91 <mosinski@hotmail.co.uk>
2020-01-29 15:54:39 +0100

    path guard service added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { AuthGuard } from './user/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  { path: '', component: HomePageComponent, canActivate: [AuthGuard] },
   // Lazy Loading setup
   {
     path: 'login', loadChildren: () => import('./user/user.module')
