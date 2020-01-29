1f36383af437599e6e4c95b10cec28eabf51f73e
michalosinski91 <mosinski@hotmail.co.uk>
2020-01-29 14:47:57 +0100

    google signin added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  //Lazy Loading setup
+  // Lazy Loading setup
   {
     path: 'login', loadChildren: () => import('./user/user.module')
     .then(m => m.UserModule)
