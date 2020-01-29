3851563e7b9beccf7a4a9d0a1d9fe5b5cc30f4c3
michalosinski91 <mosinski@hotmail.co.uk>
2020-01-29 14:24:43 +0100

    home page set up + firebase configured

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB3U6hEmxDdV9qqIFaO1Wros5S45krKb2k',
+    authDomain: 'kanban-ang.firebaseapp.com',
+    databaseURL: 'https://kanban-ang.firebaseio.com',
+    projectId: 'kanban-ang',
+    storageBucket: 'kanban-ang.appspot.com',
+    messagingSenderId: '874763545593',
+    appId: '1:874763545593:web:7bd92f0dbe31c363422b28',
+    measurementId: 'G-JHEVX3VKWJ'
+  }
 };
