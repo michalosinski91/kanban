1783218854cccfa273492e176cbf5ba1b40f9caf
michalosinski91 <mosinski@hotmail.co.uk>
2020-01-30 12:36:39 +0100

    Add, update & delete functionality added

---
+++ src/app/kanban/board-dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board-dialog',
+  template: `
+    <p>
+      board-dialog works!
+    </p>
+  `,
+  styles: []
+})
+export class BoardDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/kanban/dialogs/task-dialog.component.ts
@@ -0,0 +1,79 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BoardService } from '../board.service';
+
+@Component({
+  selector: 'app-task-dialog',
+  template: `
+    <h1 mat-dialog-title>Task</h1>
+    <div mat-dialog-content class="content">
+      <mat-form-field>
+        <textarea
+          placeholder="Task description"
+          matInput
+          [(ngModel)]="data.task.description"
+        ></textarea>
+      </mat-form-field>
+      <br />
+      <mat-button-toggle-group
+        #group="matButtonToggleGroup"
+        [(ngModel)]="data.task.label"
+      >
+        <mat-button-toggle *ngFor="let opt of labelOptions" [value]="opt">
+          <mat-icon [ngClass]="opt">
+            {{ opt === 'gray' ? 'check_circle' : 'lens'}}
+          </mat-icon>
+        </mat-button-toggle>
+      </mat-button-toggle-group>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button [mat-dialog-close]="data" cdkFocusInitial>
+        {{ data.isNew ? 'Add Task' : 'Update Task' }}
+      </button>
+      <app-delete-button
+        (delete)="handleTaskDelete()"
+        *ngIf="!data.isNew"
+      >
+      </app-delete-button>
+    </div>
+  `,
+  styles: [`
+    .content {
+      overflow: hidden;
+      height: auto;
+      padding: 20px;
+      width: 100%;
+    }
+    mat-form-field {
+      width: 100%;
+    }
+    textarea {
+      display: block;
+      width: 100%;
+    }
+    .blue { color: #71deff; }
+    .green { color: #36e9b6; }
+    .yellow { color: #ffcf44; }
+    .purple { color: #b15cff; }
+    .gray { color: gray; }
+    .red { color: #e74a4a; }
+  `]
+})
+export class TaskDialogComponent {
+  labelOptions = ['purple', 'blue', 'green', 'yellow', 'red', 'gray'];
+  constructor(
+    public dialogRef: MatDialogRef<TaskDialogComponent>,
+    private ps: BoardService,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  handleTaskDelete() {
+    this.ps.removeTask(this.data.boardId, this.data.task);
+    this.dialogRef.close();
+  }
+
+}
