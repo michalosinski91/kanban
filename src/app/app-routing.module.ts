fd7dd93eb9b994a4f7a9462df10d1399d71fb1b3
michalosinski91 <mosinski@hotmail.co.uk>
2020-01-30 10:37:50 +0100

    kanban module & DB model added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,16 @@ import { AuthGuard } from './user/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomePageComponent },
   // Lazy Loading setup
   {
     path: 'login', loadChildren: () => import('./user/user.module')
     .then(m => m.UserModule)
+  },
+  {
+    path: 'kanban', loadChildren: () => import('./kanban/kanban.module')
+    .then(m => m.KanbanModule),
+    canActivate: [AuthGuard]
   }
 ];
 
